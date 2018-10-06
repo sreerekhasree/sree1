88e04d951821dae8e6d441e58f1c22f3e10a814b
sreerekhasree <43902520+sreerekhasree@users.noreply.github.com>
2018-10-06 14:57:08 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    double b,d,e,f,g;
+    float c;
+    a=200;b=10.500000000001;c=13213.11111f;
+    d=a+b+c;
+    e=a-b;
+    f=b*c;
+    g=a/b;
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(g);
+  }
+}

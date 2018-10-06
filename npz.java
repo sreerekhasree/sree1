8672a26a8dfb9719a5f3f1209cb8ab55e6cd576b
sreerekhasree <43902520+sreerekhasree@users.noreply.github.com>
2018-10-06 15:04:30 +0530

    Update npz.java

--- npz.java
+++ npz.java
@@ -3,13 +3,13 @@ class npz {
   public static void main(String[] args) {
     int num;
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter any integer number:");
+    //System.out.println("Enter any integer number:");
     num=sc.nextInt();
     if(num>0)
-    System.out.println(num + "is POSITIVE NUMBER.");
+    System.out.println("POSITIVE");
     else if(num<0)
-    System.out.println(num + "is NEGATIVE NUMBER");
+    System.out.println("negative");
     else if(num==0)
-    System.out.println("IT's ZERO.");
+    System.out.println("ZERO");
   }
 }
